4445db5d5db9bd878c4799f4b9e0c26f9cb65e6f
matttodd <matttodd6@gmail.com>
2019-11-19 11:06:20 -0500

    don't crash

--- popup.js
+++ popup.js
@@ -25,29 +25,39 @@
 //     document.getElementById("demo").innerHTML = "EXPIRED";
 //   }
 // }, 1000);
+console.log("whatthefukc");
 
 function startTimer() {
+  // console.log("bang")
   var hours = document.getElementById("hours").value;
   var minutes = document.getElementById("minutes").value;
-  var countDownDate = new Date().getTime();
-  countDownDate.setHours(countDownDate.setHours() + hours)
-  countDownDate.setMinutes(countDownDate.setMinutes() + minutes)
+  var countDownDate = new Date();
+  console.log(countDownDate)
+  // console.log(countDownDate.getMinutes())
+  // countDownDate.setHours(countDownDate.getHours() + hours, countDownDate.getMinutes() + minutes)
+  // countDownDate.setMinutes()
+  countDownDate = new Date(countDownDate.getTime() + (hours * 60 * 60000) + (minutes * 60000))
+  console.log(countDownDate)
+  // console.log(hours)
+  // console.log(minutes)
   var x = setInterval(function() {
 
     // Get today's date and time
-    var now = new Date().getTime();
+    var now = new Date();
   
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
+    console.log(countDownDate)
+    console.log(now)
+    console.log(distance)
   
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
     // Display the result in the element with id="demo"
-    document.getElementById("demo").textContent = days + "d " + hours + "h "
+    document.getElementById("demo").textContent = hours + "h "
     + minutes + "m " + seconds + "s ";
   
     // If the count down is finished, write some text
@@ -56,4 +66,11 @@ function startTimer() {
       document.getElementById("demo").innerHTML = "EXPIRED";
     }
   }, 1000);
+}
+
+window.onload = function() {
+  console.log(startTimer)
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("submit").onclick = startTimer;
+  });
 }
