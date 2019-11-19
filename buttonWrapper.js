dc86c49f7ff4b7c6a4c3bd879964185d322ba007
matttodd <matttodd6@gmail.com>
2019-11-19 15:15:33 -0500

    this shit almost works

--- buttonWrapper.js
+++ buttonWrapper.js
@@ -75,33 +75,36 @@ function startTimer(hours, minutes) {
     // console.log(minutes)
     var x = setInterval(function() {
   
-      // Get today's date and time
-      var now = new Date();
+    // Get today's date and time
+    var now = new Date();
     
-      // Find the distance between now and the count down date
-      var distance = countDownDate - now;
-      console.log(countDownDate)
-      console.log(now)
-      console.log(distance)
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    console.log(countDownDate)
+    console.log(now)
+    console.log(distance)
     
-      // Time calculations for days, hours, minutes and seconds
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-      // Display the result in the element with id="demo"
-      var text = hours + "h "
-      + minutes + "m " + seconds + "s ";
+    // Display the result in the element with id="demo"
+    var text = hours + "h " + minutes + "m " + seconds + "s ";
     
-      // If the count down is finished, write some text
-      if (distance < 0) {
+    // If the count down is finished, write some text
+    if (distance < 0) {
         clearInterval(x);
         text = "EXPIRED";
-      }
-      chrome.runtime.sendMessage({
-          msg: "send_timer",
-          timer: text
-      });
+    } else {
+        //lights
+    }
+    var body = {
+        msg: "send_timer",
+        timer: text
+    }
+    console.log(body);
+      chrome.runtime.sendMessage(body);
     }, 1000);
   }
 
--- popup.js
+++ popup.js
@@ -1,89 +1,22 @@
-// var countDownDate = new Date("Nov 17, 2019 23:47:25").getTime();
-
-// // Update the count down every 1 second
-// var x = setInterval(function() {
-
-//   // Get today's date and time
-//   var now = new Date().getTime();
-
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
-
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//   // Display the result in the element with id="demo"
-//   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-//   + minutes + "m " + seconds + "s ";
-
-//   // If the count down is finished, write some text
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("demo").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
-console.log("whatthefukc");
-
-function startTimer() {
-  // console.log("bang")
-  var hours = document.getElementById("hours").value;
-  var minutes = document.getElementById("minutes").value;
-  var countDownDate = new Date();
-  console.log(countDownDate)
-  // console.log(countDownDate.getMinutes())
-  // countDownDate.setHours(countDownDate.getHours() + hours, countDownDate.getMinutes() + minutes)
-  // countDownDate.setMinutes()
-  countDownDate = new Date(countDownDate.getTime() + (hours * 60 * 60000) + (minutes * 60000))
-  console.log(countDownDate)
-  // console.log(hours)
-  // console.log(minutes)
-  var x = setInterval(function() {
-
-    // Get today's date and time
-    var now = new Date();
-  
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    console.log(countDownDate)
-    console.log(now)
-    console.log(distance)
-  
-    // Time calculations for days, hours, minutes and seconds
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-    // Display the result in the element with id="demo"
-    document.getElementById("demo").textContent = hours + "h "
-    + minutes + "m " + seconds + "s ";
-  
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-  }, 1000);
-}
-
 window.onload = function() {
   // console.log(startTimer)
-  document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("submit").onclick = sendStart;
-  });
 }
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse){
-      if(request.msg == "send_timer") {
-        document.getElementById("demo").textContent = request.text;
+    console.log(request)
+    if(request.msg == "send_timer") {
+      document.getElementById("demo").innerHTML = request.timer;
+      if(request.timer == "EXPIRED") {
+        document.getElementById("submit").disabled = false;
       }
+    }
   }
 );
 
 function sendStart() {
+  document.getElementById("submit").disabled = true;
   var hours = document.getElementById("hours").value;
   var minutes = document.getElementById("minutes").value;
   chrome.runtime.sendMessage({
